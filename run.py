55fca21481fd82dd0865f59ef30e648b828a9e80
Mr-Patty <shevchenko.cmc@gmail.com>
2021-09-19 14:48:20 +0300

    Added notebooks

---
+++ run.py
@@ -0,0 +1,65 @@
+import argparse
+import torch
+import pickle
+import onnx
+import onnxruntime
+
+import numpy as np
+import soundfile as sf
+
+from sklearn.metrics import classification_report
+from models.vad_models import LSTMModel
+from utils.processing import *
+from tqdm import tqdm
+
+
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test_audio', help='Path to test audio file')
+    parser.add_argument('--checkpoint', default='data/vad.pt', help='Path to saved checkpoint')
+    parser.add_argument('--device', default='cuda', help='device for pytorch model')
+    parser.add_argument('--type', default='torch', help='type of model onnx or torch')
+    parser.add_argument('--threshold', default='0.7', help='threshold for model')
+    namespace = parser.parse_args()
+    argv = vars(namespace)
+
+    model_type = argv['type']
+    if model_type == 'torch':
+        model = LSTMModel(inpud_dim=40, hidden_dim=64, n_layers=2, dropout=0.5).float()
+        model.load_state_dict(torch.load(argv['checkpoint']))
+
+    elif model_type == 'onnx':
+        onnx_model = onnx.load(argv['checkpoint'])
+        onnx.checker.check_model(onnx_model)
+        model = argv['checkpoint']
+    else:
+        print('model type must be onnx or torch')
+        return
+
+    audio_file = argv['test_audio']
+    device = argv['device']
+
+    if device == 'cuda':
+        providers = ['CUDAExecutionProvider']
+    else:
+        providers = ['CPUExecutionProvider']
+    thr = float(argv['threshold'])
+
+    data, samplerate = sf.read(argv['test_audio'])
+    features = process_audio(data)
+    if model_type == 'torch':
+        model.to(device)
+        output = to_numpy(model(torch.unsqueeze(features, 0).float().to(device))).reshape(features.shape[0])
+    elif model_type == 'onnx':
+        ort_session = onnxruntime.InferenceSession(model, providers=providers)
+        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(torch.unsqueeze(features, 0))}
+        ort_outs = ort_session.run(None, ort_inputs)
+        output = ort_outs[0].reshape(features.shape[0])
+
+
+    output = (output > thr).astype(int)
+    print(output)
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import argparse
 import torch
 import pickle
+import onnx
+import onnxruntime
 
 import numpy as np
 import torch.utils.data as data_utils
@@ -9,6 +11,10 @@ from sklearn.metrics import classification_report
 from models.vad_models import LSTMModel
 from utils.processing import *
 from tqdm import tqdm
+
+
+def to_numpy(tensor):
+    return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
 
 class CustomDataset(torch.utils.data.Dataset):
@@ -25,34 +31,38 @@ class CustomDataset(torch.utils.data.Dataset):
         return len(self.samples)
 
     
-def testModel(model, test_X, test_y, device='cuda', model_type='torch'):
-    
+def testModel(model, test_X, test_y, device='cuda', model_type='torch', providers=['CPUExecutionProvider']):
     
     test_dataset = CustomDataset(test_X, test_y)
 
     print('DATASET SIZE: {}'.format(len(test_dataset)))
-    
+    preds = []
     if model_type == 'torch':
         model.to(device)
         model.eval()
-        preds = []
         for x, _ in tqdm(test_dataset):
 
             with torch.no_grad():
                 output = model(torch.unsqueeze(x, 0).float().to(device))
                 pred = output.cpu().detach().numpy()
                 preds.extend(pred)
-    
+    else:
+        ort_session = onnxruntime.InferenceSession(model, providers=providers)
+        for x, _ in tqdm(test_dataset):
+            ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(torch.unsqueeze(x, 0))}
+            ort_outs = ort_session.run(None, ort_inputs)
+            preds.append(ort_outs[0].squeeze(0))
     return test_y, preds
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--test_path', default='test', help='Path to test data')
-    parser.add_argument('--checkpoint', default='pretrainModel', help='Path to saved checkpoint')
+    parser.add_argument('--checkpoint', default='data/vad.pt', help='Path to saved checkpoint')
     parser.add_argument('--number', default='1000', help='Number of test samples')
     parser.add_argument('--device', default='cuda', help='device')
-    parser.add_argument('--type', default='onnx', help='type of model onnx or torch')
+    parser.add_argument('--type', default='torch', help='type of model onnx or torch')
+    parser.add_argument('--threshold', default='0.7', help='threshold for model')
     namespace = parser.parse_args()
     argv = vars(namespace)
     
@@ -61,8 +71,9 @@ if __name__ == '__main__':
         model = LSTMModel(inpud_dim=40, hidden_dim=64, n_layers=2, dropout=0.5).float()
         model.load_state_dict(torch.load(argv['checkpoint']))
     elif model_type == 'onnx':
-        # todo
-        return
+        onnx_model = onnx.load(argv['checkpoint'])
+        onnx.checker.check_model(onnx_model)
+        model = argv['checkpoint']
     else:
         print('model type must be onnx or torch')
         return
@@ -70,12 +81,17 @@ if __name__ == '__main__':
     max_samples = int(argv['number'])
     test_path = argv['test_path']
     device = argv['device']
+    thr = float(argv['threshold'])
     
     with open('dev.pkl', 'rb') as f:
-        dev_samples = pickle.load(f) 
+        dev_samples = pickle.load(f)
+
+    if device == 'cuda':
+        providers = ['CUDAExecutionProvider']
+    else:
+        providers = ['CPUExecutionProvider']
+    test_X, test_y = get_test_samples(max_samples, test_path, dev_samples)
+    target, pred = testModel(model, test_X, test_y, device, model_type=model_type, providers=providers)
     
-    test_X, test_y = getTestSamples(max_samples, test_path, dev_samples)
-    target, pred = testModel(model, test_X, test_y, device)
-    
-    tmp_pred = np.vstack(pred).squeeze(1) > 0.5
+    tmp_pred = np.vstack(pred).squeeze(1) > thr
     print(classification_report(np.hstack(target), tmp_pred.astype(int)))
